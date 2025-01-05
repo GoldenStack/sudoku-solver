254ec757961f51900ee8910ad76ebc56b47db740
GoldenStack <git@goldenstack.net>
2025-01-04 19:01:10 -0600

    Benchmark average time

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -5,16 +5,43 @@ const examples = @import("examples.zig");
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
 
-    var raw_board = sudoku.board_from_tiles(examples.EASY);
-    const board = &raw_board;
+    // Verify that the code can actually solve the examples
+    try stdout.print("Verified Easy: {any}\n", .{examples.verify_example(examples.EASY, examples.EASY_SOLUTION)});
+    try stdout.print("Verified Medium: {any}\n", .{ examples.verify_example(examples.MEDIUM, examples.MEDIUM_SOLUTION) });
+    try stdout.print("Verified Hard: {any}\n", .{ examples.verify_example(examples.HARD, examples.HARD_SOLUTION) });
 
-    if (!sudoku.solve(board)) {
-        std.debug.print("SOMETHING WENT VERY WRONG!!!\n", .{});
+    // Test settings
+    const warmup_iterations = 50_000;
+    const iterations = 200_000;
+
+    const test_case = examples.EASY;
+
+    // Warmup rounds
+    try stdout.print("Warming up...\n", .{});
+    for (0..warmup_iterations) |_| {
+        var raw_board = sudoku.board_from_tiles(test_case);
+        const board = &raw_board;
+        _ = sudoku.solve(board);
     }
 
-    // const result = examples.verify_example(examples.EASY, examples.EASY_SOLUTION);
+    // Actual testing
+    try stdout.print("Starting iterations...\n", .{});
+    const start = std.time.nanoTimestamp();
+    for (0..iterations) |_| {
+        var raw_board = sudoku.board_from_tiles(test_case);
+        const board = &raw_board;
+        _ = sudoku.solve(board);
+    }
 
-    // try stdout.print("{any}", .{result});
+    const duration = std.time.nanoTimestamp() - start;
 
-    _ = stdout;
+    const duration_float = @as(f64, @floatFromInt(duration));
+    const iterations_float = @as(f64, @floatFromInt(iterations));
+
+    const ns_per_iter = duration_float / iterations_float;
+    const duration_ms = duration_float / 1e6;
+    const per_second = 1e9 / ns_per_iter;
+
+    try stdout.print("Average time for {} iterations: {d:.3}µs (took {d:.3}ms; {d:.0}/sec)\n", .{iterations, ns_per_iter / 1000, duration_ms, per_second});
+
 }
