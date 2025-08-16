17b08f119d69afaf5c8a59ed0bff75ad1d59eca4
GoldenStack <git@goldenstack.net>
2025-08-16 02:06:39 -0500

    Add more extensive documentation and remove unnecessary bitwise operations

--- zig/src/sudoku.zig
+++ zig/src/sudoku.zig
@@ -65,48 +65,73 @@ pub fn board_from_tiles(tiles: [81]u8) Board {
     return board.*;
 }
 
-pub fn set(board: *Board, index: usize, mask: Tile) bool {
+/// Sets the value of the given tile `index` to the provided mask. The mask
+/// **must** have a population count of 1.
+fn set(board: *Board, index: usize, mask: Tile) bool {
+    std.debug.assert(@popCount(mask) == 1);
     board[index] = mask;
-    return set_neighbors(board, index, mask);
+    return @call(.always_tail, set_neighbors, .{board, index, mask});
 }
 
-pub fn set_neighbors(board: *Board, index: usize, mask: Tile) bool {
+/// Recursively sets the neighbors for the provided tile `index`. This does not
+/// set the value of the tile itself.
+fn set_neighbors(board: *Board, index: usize, mask: Tile) bool {
+    // Iterate through the 20 pre-calculated neighbors
     inline for (Neighbors[index]) |neighbor| {
+        // Find the old value of the neighbor.
         const old = board[neighbor];
-        const new = old & ~mask & 0b111111111;
 
+        // Find the new value of the neighbor by unsetting the mask bit. This
+        // previously had `& 0b111111111` at the end, but if `old` never has a
+        // bit more significant than the ninth, it's not possible for this to
+        // set any.
+        const new = old & ~mask;
+
+        // If there are zero set bits after this, the board cannot be solved.
         if (new == 0) return false;
+
+        // Make sure that the tile was actually changed, and then update it.
         if (new != old) {
             board[neighbor] = new;
 
-            // TODO: Potentially optimize further by having a list of neighbors for every tile that EXCLUDES every given tile. would save one iteration
-            if (new & (new - 1) == 0 and ((old ^ mask) & ((old ^ mask) - 1)) == 0 and !set_neighbors(board, neighbor, new)) {
+            // If `@popCount(new) == 1` (this is a method to check for it), we
+            // have now solved this tile, so propagate the neighbor setting. If
+            // this neighbor setting fails, we propagate that up (the board
+            // cannot be solved).
+            if (new & (new - 1) == 0 and !set_neighbors(board, neighbor, new)) {
                 return false;
             }
         }
     }
 
+    // If nothing bad happened it must have been a success!
     return true;
 }
 
+/// Recursively brute-force solves a board by trying each possible valid number
+/// for the most narrowed-down cell.
 pub fn solve(board: *Board) bool {
+    // Search for the unsolved (>=2 possible values) tile with the least
+    // possible values. This has a fast path for any cell with two possible
+    // values left.
     var best_tile: ?usize = null;
     var least_ones: usize = 127; // Arbitrary value
 
     for (0..81) |index| {
         const ones_in_tile = @popCount(board[index]);
 
+        // If 2 <= possible values < current best, update it.
         if (ones_in_tile > 1 and ones_in_tile < least_ones) {
             best_tile = index;
             least_ones = ones_in_tile;
-            if (ones_in_tile == 2) break;
+            if (ones_in_tile == 2) break; // Fast path for the smallest possible
         }
     }
 
-    if (best_tile == null) {
-        return true;
-    }
+    // If there's no tile, the board is solved.
+    if (best_tile == null) return true;
 
+    // Make a copy of the board to restore later.
     const old_board = board.*;
 
     const tile = best_tile.?;
@@ -116,10 +141,11 @@ pub fn solve(board: *Board) bool {
         // Extract the first set bit as a mask.
         // This should compile to `blsi` on x86_64, which is rather fast.
         // This is inspired by a blog post from Daniel Lemire, adapted to
-        // unsigned integers via twos complement.
+        // unsigned integers via two's complement.
         // https://lemire.me/blog/2018/02/21/iterating-over-set-bits-quickly/
         const set_bit = value & (~value + 1);
 
+        // If we can solve the board with this change, just return it.
         if (set(board, tile, set_bit) and solve(board)) {
             return true;
         }
@@ -129,5 +155,6 @@ pub fn solve(board: *Board) bool {
         value ^= set_bit;
     }
 
-    return false; // No possibilities resulted in a solve. Rip!
+    // The given board was impossible.
+    return false;
 }
